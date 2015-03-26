4675e7a7520e20bce37a7c96e978988d7c18b103
Dan Stoppelman <dstop75@gmail.com>
2015-03-26 10:34:06 -0400

    palindrome solution

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,1 +1,14 @@
 var palindrome = {};
+
+palindrome.count = function(phrases) {
+    var count = 0;
+    phrases.forEach(function(phrase) {
+        var concat = phrase.split(' ').join('');
+        var lower = concat.toLowerCase();
+        var reverse = lower.split('').reverse().join('');
+        if (lower === reverse) {
+            count += 1;
+        }
+    });
+    return count;
+};
